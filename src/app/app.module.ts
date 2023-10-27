c3858110098d0f0454c0e8b4c858156de2d0cd84
alv <adlv@test.com>
2023-10-27 09:28:54 +0200

    création des composants header, coins, coins-modal et ajout des liens CDN bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CoinsComponent } from './coins/coins.component';
+import { CoinsModalComponent } from './coins-modal/coins-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CoinsComponent,
+    CoinsModalComponent
   ],
   imports: [
     BrowserModule,
